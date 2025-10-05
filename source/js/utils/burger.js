d69131fa306ea334cee4135005b689684c6bcaed
Alexander Kovalev <a.kovalev199@yandex.ru>
2025-10-05 17:39:59 +0500

    Добавляет часть логики для бургер-меню.

--- source/js/utils/burger.js
+++ source/js/utils/burger.js
@@ -1,6 +1,16 @@
 
 const burgerToggle = () => {
+  const burgerButton = document.querySelector('[data-validate="burger-toggle"]');
+  const burgerMenu = document.querySelector('[data-validate="burger-menu"]');
 
+  if (burgerButton && burgerMenu) {
+    burgerButton.addEventListener('click', () => {
+      const isActive = burgerMenu.getAttribute('data-active') === 'true';
+      burgerMenu.setAttribute('data-active', !isActive);
+      burgerMenu.setAttribute('aria-hidden', isActive);
+      burgerButton.setAttribute('aria-label', isActive ? 'Открыть меню' : 'Закрыть меню');
+    });
+  }
 };
 
 export {burgerToggle};
