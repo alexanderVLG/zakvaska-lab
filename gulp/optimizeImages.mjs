40b8c567f7bcd58559cf704166985bc151d9e49d
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-09-08 07:58:24 +0500

    меняет название блока.

--- gulp/optimizeImages.mjs
+++ gulp/optimizeImages.mjs
@@ -10,10 +10,10 @@ import rename from 'gulp-rename';
 
 const sprite = () =>
   gulp
-      .src('source/img/sprite/*.svg')
-      .pipe(svgstore({inlineSvg: true}))
-      .pipe(rename('sprite.svg'))
-      .pipe(gulp.dest('build/img'));
+    .src('source/img/sprite/*.svg')
+        .pipe(svgstore({inlineSvg: true}))
+        .pipe(rename('sprite.svg'))
+        .pipe(gulp.dest('build/img'));
 
 const optimizeSvg = () =>
   gulp
@@ -52,7 +52,7 @@ const optimizePng = () =>
 */
 
 const createWebp = () => {
-  const root = 'content/';
+  const root = '';
   return gulp
       .src(`source/img/${root}**/*.{png,jpg}`)
       .pipe(webp({quality: 90}))
